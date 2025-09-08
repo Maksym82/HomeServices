de2b4320c02343fee97b4fff0dda83b4d94655e9
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-09-08 21:30:49 +0300

    Update styles reviews

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -3,9 +3,14 @@ const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
 
+ // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true
   },
 
   // Navigation arrows
