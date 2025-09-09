e26b7c5582c76ca193b0ca78e906fffc19d95d11
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-09-09 09:51:48 +0300

    Update styles swiper reviews

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -1,22 +1,68 @@
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: true,
+
+//  // Default parameters
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+// });
+
+// const swiper = new Swiper(".swiper", {
+//   direction: "horizontal",
+//   loop: true,
+//   speed: 600,
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: false,
+//   },
+
+//   effect: "slide", // можно заменить на 'fade' или 'creative'
+// });
+
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
   loop: true,
-
- // Default parameters
+  speed: 600,
   slidesPerView: 1,
-  spaceBetween: 10,
+  spaceBetween: 30,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<span class="${className} custom-bullet"></span>`;
+    },
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
 });
+
