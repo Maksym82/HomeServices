566df99ea237e8600323bc4bc777624c46dee70f
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-09-09 10:15:38 +0300

    Update reviews-slider.js

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -1,51 +1,3 @@
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   direction: "horizontal",
-//   loop: true,
-
-//  // Default parameters
-//   slidesPerView: 1,
-//   spaceBetween: 10,
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-// });
-
-// const swiper = new Swiper(".swiper", {
-//   direction: "horizontal",
-//   loop: true,
-//   speed: 600,
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   autoplay: {
-//     delay: 5000,
-//     disableOnInteraction: false,
-//   },
-
-//   effect: "slide", // можно заменить на 'fade' или 'creative'
-// });
-
 const swiper = new Swiper(".swiper", {
   loop: true,
   speed: 600,
@@ -64,5 +16,22 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
 
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      translate: ["-120%", 0, -500],
+      opacity: 0,
+    },
+    next: {
+      translate: ["120%", 0, -500],
+      opacity: 0,
+    },
+  },
+
+});
