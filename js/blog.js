98cc31f2cb9edab2231660070532ddc09f27b4f6
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-09-10 20:29:50 +0300

    Create blog.js

---
+++ js/blog.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("toggleBtn");
+const hiddenText = document.querySelector(".hidden-text");
+const dots = document.querySelector(".dots");
+
+let expanded = false;
+
+btn.addEventListener("click", () => {
+  expanded = !expanded;
+  hiddenText.style.display = expanded ? "inline" : "none";
+  dots.style.display = expanded ? "none" : "inline";
+  btn.textContent = expanded ? "Скрыть" : "Читать далее";
+});
