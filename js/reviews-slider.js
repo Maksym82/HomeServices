23390b5c2fec37de1ec27a5eeafb05d680a0054c
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-09-30 19:08:04 +0300

    Update styles

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -1,8 +1,48 @@
+// const swiper = new Swiper(".swiper", {
+//   loop: true,
+//   speed: 600,
+//   grabCursor: true,
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     renderBullet: function (index, className) {
+//       return `<span class="${className} custom-bullet"></span>`;
+//     },
+//   },
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: false,
+//   },
+
+//   effect: "creative",
+//   creativeEffect: {
+//     prev: {
+//       translate: ["-120%", 0, -500],
+//       opacity: 0,
+//     },
+//     next: {
+//       translate: ["120%", 0, -500],
+//       opacity: 0,
+//     },
+//   },
+
+// });
+
 const swiper = new Swiper(".swiper", {
   loop: true,
   speed: 600,
-  slidesPerView: 1,
+  grabCursor: true,
   spaceBetween: 10,
+  slidesPerView: 1,
 
   pagination: {
     el: ".swiper-pagination",
@@ -22,15 +62,26 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: false,
   },
 
-  effect: "creative",
-  creativeEffect: {
-    prev: {
-      translate: ["-120%", 0, -500],
-      opacity: 0,
+  // effect: "creative",
+  // creativeEffect: {
+  //   prev: {
+  //     translate: ["-120%", 0, -500],
+  //     opacity: 0,
+  //   },
+  //   next: {
+  //     translate: ["120%", 0, -500],
+  //     opacity: 0,
+  //   },
+  // },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    next: {
-      translate: ["120%", 0, -500],
-      opacity: 0,
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
   },
 });
