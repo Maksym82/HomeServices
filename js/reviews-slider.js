f23c8a56870522999e5b9d78a275af90d23c2dda
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-10-02 14:08:01 +0300

    fix

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -1,41 +1,5 @@
-// const swiper = new Swiper(".swiper", {
-//   loop: true,
-//   speed: 600,
-//   grabCursor: true,
-//   slidesPerView: 1,
-//   spaceBetween: 10,
 
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//     renderBullet: function (index, className) {
-//       return `<span class="${className} custom-bullet"></span>`;
-//     },
-//   },
 
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   autoplay: {
-//     delay: 5000,
-//     disableOnInteraction: false,
-//   },
-
-//   effect: "creative",
-//   creativeEffect: {
-//     prev: {
-//       translate: ["-120%", 0, -500],
-//       opacity: 0,
-//     },
-//     next: {
-//       translate: ["120%", 0, -500],
-//       opacity: 0,
-//     },
-//   },
-
-// });
 
 const swiper = new Swiper(".swiper", {
   loop: true,
@@ -62,17 +26,7 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: false,
   },
 
-  // effect: "creative",
-  // creativeEffect: {
-  //   prev: {
-  //     translate: ["-120%", 0, -500],
-  //     opacity: 0,
-  //   },
-  //   next: {
-  //     translate: ["120%", 0, -500],
-  //     opacity: 0,
-  //   },
-  // },
+ 
 
   breakpoints: {
     768: {
